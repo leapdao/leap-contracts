7cee3a2593f22960d976d420de38fee409597e27
Johann Barbie <johannbarbie@me.com>
2018-01-24 17:47:11 +0100

    added pitch

---
+++ PITCHME.md
@@ -0,0 +1,145 @@
+
+
+# what is scalability?
+
+- x scales if it performs equally well as it grows
+- blockchains: every full node has to process every block
+- adding nodes does not help
+
+---
+
+# Scalability in ethereum
+
+- Casper: scale state space and trnsaction via sharding
+- raiden: scale transactions via state channels
+- truebit: scale computation via interactive verification
+
+only sharding requires fork
+
+---
+
+# Why Scaleable Computation
+
+Ethereum smart contracts
+- witout gas limit
+- written in any programming language
+- driven by neural networks
+- with filesystem access to swarm
+- link multiple blockchains (dogecoin)
+- verify golem computations
+- verify live video encoding (livepeer)
+
+---
+
+# TrueBit has Unanimous Consensus
+
+Computation Task -> someone runs the task -> puts the result on chain
+
+- single honest virifier can outrule anyone ("99.9% attack")
+- presence of honest verifier ensured by economic incentives
+
+Difference to Golem / iExec / Sonm:
+- Truebit: scaling up verified computation
+- others: using blockchain to pay cloud computation
+
+---
+
+# How to Scale Computation
+
+- only few people perform computation off-chain
+- if anyone disagrees, they go to court (blockchain)
+- on-chain settling must be magnitudes faster
+
+---
+
+# Verificatio Game
+
+Parties compute Merkle-trees of full state (memory) at every single computation step and submit roots at certain steps
+
+step: 1						1 000 000
+Proposer: 	input			output_1
+Challenger: input 			output_2
+
+judge asks for merkle root in middle of exkecution
+step 		500 000
+Proposer: 	0x1234
+Challenger 	0x1234
+
+binary search continues:
+step 		750 000		625 000
+Proposer: 	0x7328		0x8256
+Challenger 	0x1862		0x8256
+
+=> step in agreement, next step not in agreement
+step 		638 225		638 226
+Proposer: 	0x4321		0x8922
+Challenger 	0x4321		0x8923
+
+after 20 rounds: agreement -> disagremment in one step 
+both submit merkre proofs, smart contract verifies with no effort and finds cheater
+
+---
+
+# The Good News
+
+- 20 rounds can be further reduced
+- cheater is found with certainty: large disincentive to cheat
+- game will never be played but has to be there as fallback
+
+---
+
+@title[Introduction]
+
+# Git<span class="gold">Pitch</span>
+
+# TrueBit: Scalable Computation
+<br>
+<br>
+<span class="byline">[ Johann Barbie  ]</span>
+
+--- 
+
+
+# Problem: Verifier's Dilemma
+
+- only need single verifier, but has to be altruistic
+- over time, verifiers will stop looking because system works
+- system breaks down
+
+---
+
+# Solution: Forced Errors
+
+- pseudorandomly inject forced errors
+- verifiers get reward for finding forced errors
+- solver is not punished if forced errors in found
+
+---
+
+# Problem: Information Sharing
+
+- solver can notify certain veriffiers about error
+- verifiers can create multiple accounts
+- ho to ensure computation was actually run?
+
+---
+
+# Solution: Reduce Reward with every Challenge
+
+- solver can challenge
+- total error finding reward is halved for each report
+
+Payouts:
+1 challenge: 100 = 100
+2 challenges: 25 + 25 = 50
+3 challenges: 8.333 + 8.333 + 8.333 = 25
+...
+
+
+---
+
+
+
+
+
+
--- test/plasmaParent.js
+++ test/plasmaParent.js
@@ -19,21 +19,20 @@ const createFundingTransaction = require('./helpers/createFundingTransaction');
 
 contract('PlasmaParent', (accounts) => {
 
-  it('should allow to submit block', async () => {
+  it('should allow to deposit and submit block', async () => {
   	// deploy contract
     const plasmaParent = await PlasmaParent.new();
     // add operator
     await plasmaParent.setOperator('0xf3beac30c498d9e26865f34fcaa57dbb935b0d74', true);
 
+    // deposit some ETH
+    await plasmaParent.deposit({ from: accounts[1], value: new Web3.utils.BN(1000000) });
+    let record = await plasmaParent.depositRecordsForUser(accounts[1]);
+    assert(record.length > 0);
     // create deposit tx in block 0
     let txQueueArray = [];
     let depositIndexBN = new BN(0);
-    const tx = createFundingTransaction(accounts[1], new Web3.utils.BN(1000), depositIndexBN);
-    let txRaw = Buffer.concat(tx.clearRaw(false, false));
-    const privBuf = new Buffer('278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f', 'hex');
-    const hash = ethUtil.sha3(txRaw);
-    const sig = ethUtil.ecsign(hash, privBuf);
-    tx.serializeSignature(sig.r.toString('hex') + sig.s.toString('hex') + sig.v.toString(16));
+    const tx = createFundingTransaction(accounts[1], new Web3.utils.BN(1000000), depositIndexBN);
     txQueueArray.push(tx);
 
     // create first block
@@ -53,18 +52,79 @@ contract('PlasmaParent', (accounts) => {
     const blockHash = ethUtil.sha3(blockRaw);
 
     // sign block by operator
+    const privBuf = new Buffer('278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f', 'hex');
     const blockSig = ethUtil.ecsign(blockHash, privBuf);
     block.serializeSignature(blockSig.r.toString('hex') + blockSig.s.toString('hex') + blockSig.v.toString(16));
 
-    // check submitted block
+    // check previous block number
     let number = await plasmaParent.lastBlockNumber.call();
 
     // submit new block
     const hexData = ethUtil.addHexPrefix(Buffer.concat(block.header.raw).toString('hex'));
     let rv = await plasmaParent.submitBlockHeader(hexData);
 
+    // assert submission
     let newNumber = await plasmaParent.lastBlockNumber.call();
     assert.equal(number.toNumber() + 1, newNumber.toNumber());
+  });
+
+  it('should allow challenge miner', async () => {
+
+    // deploy contract
+    const plasmaParent = await PlasmaParent.new();
+    // add operator
+    await plasmaParent.setOperator('0xf3beac30c498d9e26865f34fcaa57dbb935b0d74', true);
+
+    // deposit some ETH
+    await plasmaParent.deposit({ from: accounts[1], value: new Web3.utils.BN(1000000) });
+    let record = await plasmaParent.depositRecordsForUser(accounts[1]);
+    assert(record.length > 0);
+    // create deposit tx in block 0
+    let txQueueArray = [];
+    let depositIndexBN = new BN(0);
+    const tx = createFundingTransaction(accounts[1], new Web3.utils.BN(1000000), depositIndexBN);
+    txQueueArray.push(tx);
+
+    // create first block
+    let lastBlock = ethUtil.setLengthLeft(ethUtil.toBuffer(new BN(0)), blockNumberLength);
+    let lastBlockHash = ethUtil.sha3('parsecLabs');
+    const lastBlockNumber = Web3.utils.toBN(ethUtil.addHexPrefix(lastBlock.toString('hex')));
+    const newBlockNumber = lastBlockNumber.add(new BN(1));
+    const newBlockNumberBuffer = ethUtil.setLengthLeft(ethUtil.toBuffer(newBlockNumber), blockNumberLength);
+    const blockParams = {
+        blockNumber:  newBlockNumberBuffer,
+        parentHash: lastBlockHash,
+        transactions: txQueueArray
+    }
+    const block = new Block(blockParams); 
+    let blockRaw = block.clearRaw(false);
+    blockRaw = ethUtil.bufferToHex(Buffer.concat(blockRaw));
+    const blockHash = ethUtil.sha3(blockRaw);
+
+    // sign block by operator
+    const privBuf = new Buffer('278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f', 'hex');
+    const blockSig = ethUtil.ecsign(blockHash, privBuf);
+    block.serializeSignature(blockSig.r.toString('hex') + blockSig.s.toString('hex') + blockSig.v.toString(16));
+
+    // check previous block number
+    let number = await plasmaParent.lastBlockNumber.call();
+
+    // submit new block
+    const hexData = ethUtil.addHexPrefix(Buffer.concat(block.header.raw).toString('hex'));
+    let rv = await plasmaParent.submitBlockHeader(hexData);
+
+
+
+    // create deposit tx in block 0
+    // let txQueueArray = [];
+    // let depositIndexBN = new BN(0);
+    // const tx = createFundingTransaction(accounts[1], new Web3.utils.BN(1000), depositIndexBN);
+    // let txRaw = Buffer.concat(tx.clearRaw(false, false));
+    // const privBuf = new Buffer('278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f', 'hex');
+    // const hash = ethUtil.sha3(txRaw);
+    // const sig = ethUtil.ecsign(hash, privBuf);
+    // tx.serializeSignature(sig.r.toString('hex') + sig.s.toString('hex') + sig.v.toString(16));
+    // txQueueArray.push(tx);
   });
 
 });
