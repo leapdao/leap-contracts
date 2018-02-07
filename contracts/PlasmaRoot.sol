b7f064721fb6d6aa5e72e80617bdf67496369c27
Kosta Korenkov <kosta@whoot.me>
2018-02-07 21:00:06 +0300

    submitBlock implementation

--- contracts/PlasmaRoot.sol
+++ contracts/PlasmaRoot.sol
@@ -1,17 +1,39 @@
 pragma solidity ^0.4.19;
 
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+
 contract PlasmaRoot {
+  using SafeMath for uint256;
+
+  // A list of Plasma blocks, for each block storing
+  // (i) the Merkle root,
+  // (ii) the time the Merkle root was submitted.
+  mapping(uint256 => childBlock) public childChain;
+  uint256 public currentChildBlock;
+
+  struct childBlock {
+      bytes32 root;
+      uint256 created_at;
+  }
 
   address public owner;
+  uint256 public lastParentBlock;
 
   function PlasmaRoot() public {
     owner = msg.sender;
+    currentChildBlock = 1;
   }
 
   // submits a block, which is basically just the Merkle root of the transactions
   // in the block
   function submitBlock(bytes32 root) public {
-
+    require(block.number > lastParentBlock);
+    childChain[currentChildBlock] = childBlock({
+        root: root,
+        created_at: block.timestamp
+    });
+    currentChildBlock = currentChildBlock.add(1);
+    lastParentBlock = block.number;
   }
 
   // generates a block that contains only one transaction, generating a new UTXO
--- test/plasmaRoot.js
+++ test/plasmaRoot.js
@@ -1,11 +1,27 @@
 const PlasmaRoot = artifacts.require('./PlasmaRoot.sol');
 
 contract('PlasmaRoot', (accounts) => {
+  let plasmaRoot;
+
+  before(async () => {
+    plasmaRoot = await PlasmaRoot.new();
+  });
 
   it('should have owner', async () => {
-    const plasmaRoot = await PlasmaRoot.new();
     const owner = await plasmaRoot.owner();
     assert.equal(owner, accounts[0]);
   });
+
+  it('#submitBlock', async () => {
+    const merkleRoot = 0x3133700000000000000000000000000000000000000000000000000000000000;
+
+    await plasmaRoot.submitBlock(merkleRoot);
+
+    assert.equal(await plasmaRoot.currentChildBlock(), 2);
+    assert.equal(await plasmaRoot.lastParentBlock(), web3.eth.blockNumber);
+    const childBlock = await plasmaRoot.childChain(1);
+    assert.equal(childBlock[0], merkleRoot);
+    assert.equal(childBlock[1], web3.eth.getBlock(web3.eth.blockNumber).timestamp);
+  })
 
 });
