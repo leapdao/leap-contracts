b505025eb2289f1cf2375a86a7af65c972b02ec7
Johann Barbie <johannbarbie@me.com>
2018-05-28 12:29:15 +0200

    experiments with epochs

--- contracts/StakingAuction.sol
+++ contracts/StakingAuction.sol
@@ -4,129 +4,171 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract StakingAuction {
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    event Epoch(uint256 epoch);
-    event ValidatorJoin(address indexed signerAddr, uint256 epoch);
-    event ValidatorLeave(address indexed signerAddr, uint256 epoch);
-    
-    uint256 epochLength;
-    uint256 currentEpoch;
-    ERC20 token;
-    
-    struct Slot {
-        address owner;
-        uint64 stake;
-        address signer;
-        uint32 activationEpoch;
-        address newOwner;
-        uint64 newStake;
-        address newSigner;
-    }
-    
-    mapping(uint256 => Slot) slots;
-    
-    constructor(ERC20 _token, uint256 _slotCount) public {
-        require(_slotCount < 256);
-        require(_slotCount >= 4);
-        epochLength = _slotCount;
-        require(_token != address(0));
-        token = _token;
-    }
+  event Epoch(uint256 epoch);
+  event NewHeight(uint256 blockNumber, bytes32 indexed root);
+  event ValidatorJoin(address indexed signerAddr, uint256 epoch);
+  event ValidatorLeave(address indexed signerAddr, uint256 epoch);
+  
+  bytes32 constant genesis = 0x4920616d207665727920616e6772792c20627574206974207761732066756e21; // "I am very angry, but it was fun!" @victor
+  uint256 epochLength;       // length of epoch in periods (32 blocks)
+  uint256 lastCompleteEpoch; // height at which last epoch was completed
+  uint32 public parentBlockInterval; // how often epochs can be submitted max
+  uint64 public lastParentBlock; // last ethereum block when epoch was submitted
+  bytes32 public tipHash;    // hash of first period that has extended chain to some height
+  ERC20 token;
+  
+  struct Slot {
+    address owner;
+    uint64 stake;
+    address signer;
+    uint32 activationEpoch;
+    address newOwner;
+    uint64 newStake;
+    address newSigner;
+  }
+  
+  mapping(uint256 => Slot) slots;
 
-    function getSlot(uint256 _slotId) constant public returns (address, uint64, address, uint32, address, uint64, address) {
-        require(_slotId < epochLength);
-        Slot memory slot = slots[_slotId];
-        return (slot.owner, slot.stake, slot.signer, slot.activationEpoch, slot.newOwner, slot. newStake, slot.newSigner);
-    }
+  struct Period {
+    bytes32 parent; // the id of the parent node
+    uint32 height;  // the height of last block in period
+    uint32 parentIndex; //  the position of this node in the Parent's children list
+    uint8 slot;
+    uint32 gasPrice;
+    uint64 reward;
+    bytes32[] children; // unordered list of children below this node
+  }
+  mapping(bytes32 => Period) public chain;
     
-    function buy(uint256 _slotId, uint256 _value, address _signerAddr) public {
-        require(_slotId < epochLength);
-        Slot storage slot = slots[_slotId];
-        // take care of logout
-        if (_value == 0 && slot.newStake == 0 && slot.signer == _signerAddr) {
-            slot.activationEpoch = uint32(currentEpoch.add(2));
-            return;
-        }
-        uint required = slot.stake;
-        if (slot.newStake > required) {
-            required = slot.newStake;
-        }
-        required = required.mul(105).div(100);
-        require(required < _value);
-        token.transferFrom(msg.sender, this, _value);
-        if (slot.newStake > 0) {
-            token.transfer(slot.newOwner, slot.newStake);
-        }
-        // auction
-        if (slot.stake > 0) {
-            slot.newOwner = msg.sender;
-            slot.newSigner = _signerAddr;
-            slot.newStake = uint64(_value);
-            slot.activationEpoch = uint32(currentEpoch.add(2));
-        }
-        // new purchase
-        else {
-            slot.owner = msg.sender;
-            slot.signer = _signerAddr;
-            slot.stake = uint64(_value);
-            slot.activationEpoch = 0;
-        }
+  constructor(ERC20 _token, uint256 _slotCount) public {
+    require(_slotCount < 256);
+    require(_slotCount >= 2);
+
+    Period memory genPeriod;
+    genPeriod.parent = genesis;
+    tipHash = genesis;
+    chain[tipHash] = genPeriod;
+
+    epochLength = _slotCount;
+    require(_token != address(0));
+    token = _token;
+  }
+
+  function getSlot(uint256 _slotId) constant public returns (address, uint64, address, uint32, address, uint64, address) {
+    require(_slotId < epochLength);
+    Slot memory slot = slots[_slotId];
+    return (slot.owner, slot.stake, slot.signer, slot.activationEpoch, slot.newOwner, slot. newStake, slot.newSigner);
+  }
+  
+  function bet(uint256 _slotId, uint256 _value, address _signerAddr) public {
+    require(_slotId < epochLength);
+    Slot storage slot = slots[_slotId];
+    // take care of logout
+    if (_value == 0 && slot.newStake == 0 && slot.signer == _signerAddr) {
+      slot.activationEpoch = uint32(lastCompleteEpoch.add(3));
+      return;
     }
-    
-    function activate(uint256 _slotId) public {
-        require(_slotId < epochLength);
-        Slot storage slot = slots[_slotId];
-        require(currentEpoch >= slot.activationEpoch);
-        if (slot.stake > 0) {
-            token.transfer(slot.owner, slot.stake);
-            emit ValidatorLeave(slot.signer, currentEpoch);
-        }
-        slot.owner = slot.newOwner;
-        slot.signer = slot.newSigner;
-        slot.stake = slot.newStake;
-        slot.activationEpoch = 0;
-        slot.newOwner = 0;
-        slot.newSigner = 0;
-        slot.newStake = 0;
-        emit ValidatorJoin(slot.signer, currentEpoch);
+    uint required = slot.stake;
+    if (slot.newStake > required) {
+      required = slot.newStake;
     }
-    
-    function submitBlock(uint256 _slotId, address _signer) public {
-        require(_slotId < epochLength);
-        Slot storage slot = slots[_slotId];
-        require(slot.signer == _signer);
-        if (slot.activationEpoch > 0) {
-            // if slot not active, prevent submission
-            require(currentEpoch.add(1) < slot.activationEpoch);
-        }
-        epochLength = epochLength;
+    required = required.mul(105).div(100);
+    require(required < _value);
+    token.transferFrom(msg.sender, this, _value);
+    if (slot.newStake > 0) {
+      token.transfer(slot.newOwner, slot.newStake);
     }
-    
-    function incrementEpoch() public {
-        currentEpoch = currentEpoch.add(1);
-        emit Epoch(currentEpoch);
+    // auction
+    if (slot.stake > 0) {
+      slot.newOwner = msg.sender;
+      slot.newSigner = _signerAddr;
+      slot.newStake = uint64(_value);
+      slot.activationEpoch = uint32(lastCompleteEpoch.add(3));
     }
-    
-    function slash(uint256 _slotId, uint256 _value) public {
-        require(_slotId < epochLength);
-        Slot storage slot = slots[_slotId];
-        require(slot.stake > 0);
-        uint256 prevStake = slot.stake;
-        slot.stake = (_value >= slot.stake) ? 0 : slot.stake - uint64(_value);
-        // if slot became empty by slashing
-        if (prevStake > 0 && slot.stake == 0) {
-            emit ValidatorLeave(slot.signer, currentEpoch);
-            slot.activationEpoch = 0;
-            if (slot.newStake > 0) {
-                // someone in queue
-                activate(_slotId);
-            } else {
-                // clean out account
-                slot.owner = 0;
-                slot.signer = 0;
-                slot.stake = 0;
-            }
-        }
+    // new purchase
+    else {
+      slot.owner = msg.sender;
+      slot.signer = _signerAddr;
+      slot.stake = uint64(_value);
+      slot.activationEpoch = 0;
+      emit ValidatorJoin(slot.signer, lastCompleteEpoch + 1);
     }
+  }
+  
+  function activate(uint256 _slotId) public {
+    require(_slotId < epochLength);
+    Slot storage slot = slots[_slotId];
+    require(lastCompleteEpoch + 1 >= slot.activationEpoch);
+    if (slot.stake > 0) {
+      token.transfer(slot.owner, slot.stake);
+      emit ValidatorLeave(slot.signer, lastCompleteEpoch + 1);
+    }
+    slot.owner = slot.newOwner;
+    slot.signer = slot.newSigner;
+    slot.stake = slot.newStake;
+    slot.activationEpoch = 0;
+    slot.newOwner = 0;
+    slot.newSigner = 0;
+    slot.newStake = 0;
+    emit ValidatorJoin(slot.signer, lastCompleteEpoch + 1);
+  }
+  
+  function submitPeriod(uint256 _slotId, bytes32 _prevHash, bytes32 _root) public {
+    require(_slotId < epochLength);
+    Slot storage slot = slots[_slotId];
+    require(slot.signer == msg.sender);
+
+    // check parent node exists
+    require(chain[_prevHash].parent > 0);
+    // calculate height
+    uint256 newHeight = chain[_prevHash].height + 32;
+    // check if epoch completed
+    if (newHeight >= lastCompleteEpoch.add(epochLength.mul(32))) {
+      lastCompleteEpoch++;
+    }
+    // do some magic if chain extended
+    if (newHeight > chain[tipHash].height) {
+      // new periods can only be submitted every x Ethereum blocks
+      require(block.number >= lastParentBlock + parentBlockInterval);
+      tipHash = _root;
+      lastParentBlock = uint64(block.number);
+      emit NewHeight(newHeight, _root);
+    }
+    // store the period
+    Period memory newPeriod;
+    newPeriod.parent = _prevHash;
+    newPeriod.height = uint32(newHeight);
+    newPeriod.slot = uint8(_slotId);
+    newPeriod.parentIndex = uint32(chain[_prevHash].children.push(_root) - 1);
+    chain[_root] = newPeriod;
+
+    if (slot.activationEpoch > 0) {
+      // if slot not active, prevent submission
+      require(lastCompleteEpoch.add(2) < slot.activationEpoch);
+    }
+  }
+  
+  function slash(uint256 _slotId, uint256 _value) public {
+    require(_slotId < epochLength);
+    Slot storage slot = slots[_slotId];
+    require(slot.stake > 0);
+    uint256 prevStake = slot.stake;
+    slot.stake = (_value >= slot.stake) ? 0 : slot.stake - uint64(_value);
+    // if slot became empty by slashing
+    if (prevStake > 0 && slot.stake == 0) {
+      emit ValidatorLeave(slot.signer, lastCompleteEpoch + 1);
+      slot.activationEpoch = 0;
+      if (slot.newStake > 0) {
+        // someone in queue
+        activate(_slotId);
+      } else {
+        // clean out account
+        slot.owner = 0;
+        slot.signer = 0;
+        slot.stake = 0;
+      }
+    }
+  }
 }
--- test/stakingAuction.js
+++ test/stakingAuction.js
@@ -9,38 +9,42 @@ const should = chai
   .use(require('chai-as-promised'))
   .should();
 
+const empty = '0x0000000000000000000000000000000000000000000000000000000000000000';
+
 contract('StakingAuction', (accounts) => {
   const alice = accounts[0];
   const bob = accounts[1];
   const charlie = accounts[2];
-
   let auction;
   let token;
   let totalSupply;
+  const p = [];
 
   before(async () => {
     token = await SimpleToken.new();
     // initialize contract
-    auction = await StakingAuction.new(token.address, 4);
+    auction = await StakingAuction.new(token.address, 3);
+    p[0] = await auction.tipHash();
     totalSupply = await token.totalSupply();
     token.transfer(bob, totalSupply.div(3));
     token.transfer(charlie, totalSupply.div(3));
   });
 
   it('should prevent submission by unbonded validators', async () => {
-    await auction.submitBlock(0, alice).should.be.rejectedWith(EVMRevert);
+    await auction.submitPeriod(0, p[0], empty, {from: alice}).should.be.rejectedWith(EVMRevert);
   });
 
   it('should allow to buy slot and submit block', async () => {
     await token.approve(auction.address, totalSupply, {from: alice});
-    await auction.buy(0, 100, alice, {from: alice}).should.be.fulfilled;
-    await auction.submitBlock(0, alice).should.be.fulfilled;
+    await auction.bet(0, 100, alice, {from: alice}).should.be.fulfilled;
+    await auction.submitPeriod(0, p[0], '0x01', {from: alice}).should.be.fulfilled;
+    p[1] = await auction.tipHash();
   });
 
   it('should prevent auctining for lower price', async () => {
     await token.approve(auction.address, totalSupply, {from: bob});
-    await auction.buy(0, 99, bob, {from: bob}).should.be.rejectedWith(EVMRevert);
-    await auction.buy(0, 101, bob, {from: bob}).should.be.rejectedWith(EVMRevert);
+    await auction.bet(0, 99, bob, {from: bob}).should.be.rejectedWith(EVMRevert);
+    await auction.bet(0, 101, bob, {from: bob}).should.be.rejectedWith(EVMRevert);
   });
 
   it('should allow to be slashed',  async () => {
@@ -48,39 +52,61 @@ contract('StakingAuction', (accounts) => {
   });
 
   it('should allow to auction for higer price',  async () => {
-    await auction.buy(0, 120, bob, {from: bob}).should.be.fulfilled;
+    await auction.bet(0, 120, bob, {from: bob}).should.be.fulfilled;
   });
 
   it('should allow submission when slot auctioned in same epoch', async () => {
-    await auction.submitBlock(0, alice).should.be.fulfilled;
+    await auction.submitPeriod(0, p[1], '0x02', {from: alice}).should.be.fulfilled;
+    p[2] = await auction.tipHash();
   });
 
   it('should prevent submission by auctioned slot in later epoch', async () => {
-    await auction.incrementEpoch();
-    await auction.submitBlock(0, alice).should.be.rejectedWith(EVMRevert);
-    await auction.submitBlock(0, bob).should.be.rejectedWith(EVMRevert);
+    await auction.submitPeriod(0, p[2], '0x03', {from: alice}).should.be.rejectedWith(EVMRevert);
+    await auction.submitPeriod(0, p[2], '0x03', {from: bob}).should.be.rejectedWith(EVMRevert);
+  });
+
+  it('allow to buy another slot', async () => {
+    await token.approve(auction.address, totalSupply, {from: charlie});
+    await auction.bet(1, 100, charlie, {from: charlie}).should.be.fulfilled;
   });
 
   it('should allow to activate auctioned slot and submit', async () => {
-    await auction.incrementEpoch();
+    // increment Epoch
+    await auction.submitPeriod(1, p[2], '0x03', {from: charlie}).should.be.fulfilled;
+    p[3] = await auction.tipHash();
+    await auction.submitPeriod(1, p[3], '0x04', {from: charlie}).should.be.fulfilled;
+    p[4] = await auction.tipHash();
+    await auction.submitPeriod(1, p[4], '0x05', {from: charlie}).should.be.fulfilled;
+    p[5] = await auction.tipHash();
+    // activate and submit by bob
     const bal1 = await token.balanceOf(alice);
     await auction.activate(0);
     const bal2 = await token.balanceOf(alice);
     assert.equal(bal1.add(80).toNumber(), bal2.toNumber());
-    await auction.submitBlock(0, bob).should.be.fulfilled;
+    await auction.submitPeriod(0, p[5], '0x06', {from: bob}).should.be.fulfilled;
+    p[6] = await auction.tipHash();
   });
 
   it('should allow to logout', async () => {
-    await auction.buy(0, 0, bob, {from: bob}).should.be.fulfilled;
+    await auction.bet(0, 0, bob, {from: bob}).should.be.fulfilled;
   });
 
   it('should prevent submission by logged-out slot in later epoch', async () => {
-    await auction.incrementEpoch();
-    await auction.submitBlock(0, bob).should.be.rejectedWith(EVMRevert);
+    // increment epoch
+    await auction.submitPeriod(1, p[6], '0x07', {from: charlie}).should.be.fulfilled;
+    p[7] = await auction.tipHash();
+    await auction.submitPeriod(1, p[4], '0x08', {from: charlie}).should.be.fulfilled;
+    p[8] = await auction.tipHash();
+    // try to submit when logged out
+    await auction.submitPeriod(0, p[8], '0x09', {from: bob}).should.be.rejectedWith(EVMRevert);
   });
 
   it('should allow to withdraw after logout', async () => {
-    await auction.incrementEpoch();
+    // increment epoch
+    await auction.submitPeriod(1, p[8], '0x09', {from: charlie}).should.be.fulfilled;
+    p[9] = await auction.tipHash();
+    await auction.submitPeriod(1, p[9], '0x0a', {from: charlie}).should.be.fulfilled;
+    // activate logout
     const bal1 = await token.balanceOf(bob);
     await auction.activate(0);
     const bal2 = await token.balanceOf(bob);
