807f0c89c0c3c81275fb54abec37abc56bd3f080
Alex Lunyov <isuntc@gmail.com>
2018-06-12 19:22:21 +0800

    Allow for owner to update a slot without an auction

--- test/parsecBridge.js
+++ test/parsecBridge.js
@@ -42,6 +42,14 @@ contract('Parsec', (accounts) => {
         p[1] = await parsec.tipHash();
       });
 
+      it('should update slot instead of auction for same owner', async () => {
+        await parsec.bet(2, 10, alice, alice, {from: alice}).should.be.fulfilled;
+        await parsec.bet(2, 30, alice, alice, {from: alice}).should.be.fulfilled;
+        const slot = await parsec.slots(2);
+        assert.equal(Number(slot[1]), 30); // stake === 30
+        assert.equal(Number(slot[6]), 0); // newStake === 0
+      });
+
       it('should prevent auctining for lower price', async () => {
         await token.approve(parsec.address, 1000, {from: bob});
         await parsec.bet(0, 129, bob, bob, {from: bob}).should.be.rejectedWith(EVMRevert);
@@ -492,7 +500,7 @@ contract('Parsec', (accounts) => {
 
         // check tip
         tip = await parsec.getTip();
-        assert.equal(p[1], tip[0]);        
+        assert.equal(p[1], tip[0]);
       });
     });
     describe('Deposit', function() {
