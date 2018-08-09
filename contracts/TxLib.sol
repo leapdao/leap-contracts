a6838311a5ef546f24129b7cb5afc35d58f5564a
Johann Barbie <johannbarbie@me.com>
2018-08-09 15:51:33 +0200

    improve comments

--- contracts/TxLib.sol
+++ contracts/TxLib.sol
@@ -47,24 +47,26 @@ library TxLib {
   }
   
   function parseInput(TxType _type, bytes _txData, uint256 _pos, uint256 offset, Input[] _ins) internal pure returns (uint256 newOffset) {
-    bytes32 hash;
+    bytes32 inputData;
     uint8 index;
     if (_type == TxType.Deposit) {
       assembly {
-        // TODO: explain this
-        hash := mload(add(add(offset, 4), _txData))
+        // load the depositId (4 bytes) starting from byte 2 of tx
+        inputData := mload(add(add(offset, 4), _txData))
       }
-      hash = bytes32(uint32(hash));
+      inputData = bytes32(uint32(inputData));
       index = 0;
       newOffset = offset + 4;
     } else {    
       assembly {
-          hash := mload(add(add(offset, 32), _txData))
-          index := mload(add(add(offset, 33), _txData))
+        // load the prevHash (32 bytes) from input 
+        inputData := mload(add(add(offset, 32), _txData))
+        // load the output index (1 byte) from input 
+        index := mload(add(add(offset, 33), _txData))
       }
       newOffset = offset + 33;
     }
-    Outpoint memory outpoint = Outpoint(hash, index);
+    Outpoint memory outpoint = Outpoint(inputData, index);
     Input memory input = Input(outpoint, 0, 0, 0);
     if (_type == TxType.Transfer ||
       ((_type == TxType.CompReq || _type == TxType.CompRsp ) && _pos > 0)) {
@@ -87,22 +89,22 @@ library TxLib {
   // Copies 'len' bytes from 'srcPtr' to 'destPtr'.
   // NOTE: This function does not check if memory is allocated, it only copies the bytes.
   function memcopy(uint srcPtr, uint destPtr, uint len) internal pure {
-      uint offset = 0;
-      uint size = len / WORD_SIZE;
-      // Copy word-length chunks while possible.
-      for (uint i = 0; i < size; i++) {
-          offset = i * WORD_SIZE;
-          assembly {
-              mstore(add(destPtr, offset), mload(add(srcPtr, offset)))
-          }
-      }
-      offset = size*WORD_SIZE;
-      uint mask = ONES << 8*(32 - len % WORD_SIZE);
+    uint offset = 0;
+    uint size = len / WORD_SIZE;
+    // Copy word-length chunks while possible.
+    for (uint i = 0; i < size; i++) {
+      offset = i * WORD_SIZE;
       assembly {
-          let nSrc := add(srcPtr, offset)
-          let nDest := add(destPtr, offset)
-          mstore(nDest, or(and(mload(nSrc), mask), and(mload(nDest), not(mask))))
+        mstore(add(destPtr, offset), mload(add(srcPtr, offset)))
       }
+    }
+    offset = size*WORD_SIZE;
+    uint mask = ONES << 8*(32 - len % WORD_SIZE);
+    assembly {
+      let nSrc := add(srcPtr, offset)
+      let nDest := add(destPtr, offset)
+      mstore(nDest, or(and(mload(nSrc), mask), and(mload(nDest), not(mask))))
+    }
   }
   
   function parseOutput(TxType _type, bytes _txData, uint256 _pos, uint256 offset, Output[] _outs) internal pure returns (uint256 newOffset) {
@@ -110,40 +112,40 @@ library TxLib {
     uint16 color;
     address owner;
     assembly {
-        value := mload(add(add(offset, 8), _txData))
-        color := mload(add(add(offset, 10), _txData))
-        owner := mload(add(add(offset, 30), _txData))
+      value := mload(add(add(offset, 8), _txData))
+      color := mload(add(add(offset, 10), _txData))
+      owner := mload(add(add(offset, 30), _txData))
     }
     bytes memory data = new bytes(0);
     Output memory output = Output(value, color, owner, 0, data, 0);
     _outs[_pos] = output;
     newOffset = offset + 30;
     if (_type == TxType.CompReq && _pos == 0) {
-        // read gasPrice
-        // read length of msgData
-        uint32 gasPrice;
-        assembly {
-            gasPrice := mload(add(add(offset, 34), _txData))
-            value := mload(add(add(offset, 36), _txData))
-        }
-        output.gasPrice = gasPrice;
-        // read msgData
-        value = uint16(value); // using value for length here
-        data = new bytes(value);
-        uint src;
-        uint dest;
-        assembly {
-            src := add(add(add(offset, 36), 0x20), _txData)
-            dest := add(data, 0x20)
-        }
-        memcopy(src, dest, value);
-        output.msgData = data;
-        newOffset = offset + 36 + value;
+      // read gasPrice
+      // read length of msgData
+      uint32 gasPrice;
+      assembly {
+        gasPrice := mload(add(add(offset, 34), _txData))
+        value := mload(add(add(offset, 36), _txData))
+      }
+      output.gasPrice = gasPrice;
+      // read msgData
+      value = uint16(value); // using value for length here
+      data = new bytes(value);
+      uint src;
+      uint dest;
+      assembly {
+        src := add(add(add(offset, 36), 0x20), _txData)
+        dest := add(data, 0x20)
+      }
+      memcopy(src, dest, value);
+      output.msgData = data;
+      newOffset = offset + 36 + value;
     } else if (_type == TxType.CompRsp && _pos == 0) {
-        // read new stateRoot
-        bytes32 stateRoot;
-        output.stateRoot = stateRoot; 
-        newOffset = offset + 33 + 32;
+      // read new stateRoot
+      bytes32 stateRoot;
+      output.stateRoot = stateRoot; 
+      newOffset = offset + 33 + 32;
     }
   }
     
@@ -152,42 +154,42 @@ library TxLib {
     TxType txType;
     uint256 a;
     assembly {
-        a := mload(add(0x20, _txData))
+      a := mload(add(0x20, _txData))
     }
     a = a >> 248; // get first byte
     if (a == 2 ) {
-        txType = TxType.Deposit;
+      txType = TxType.Deposit;
     } else if (a == 3 ) {
-        txType = TxType.Transfer;
+      txType = TxType.Transfer;
     } else if (a == 4 ) {
-        txType = TxType.Consolidate;
+      txType = TxType.Consolidate;
     } else if (a == 5 ) {
-        txType = TxType.CompReq;
+      txType = TxType.CompReq;
     } else if (a == 6 ) {
-        txType = TxType.CompRsp;
+      txType = TxType.CompRsp;
     } else {
-        revert("unknow tx type");
+      revert("unknow tx type");
     }
     // read ins and outs
     assembly {
         a := mload(add(0x21, _txData))
     }
-    a = a >> 252; // get ins nibble
+    a = a >> 252; // get ins-length nibble
     Input[] memory ins = new Input[](a);
     uint256 offset = 2;
     for (uint i = 0; i < ins.length; i++) {
-        offset = parseInput(txType, _txData, i, offset, ins);
+      offset = parseInput(txType, _txData, i, offset, ins);
     }
     assembly {
         a := mload(add(0x21, _txData))
     }
-    a = (a >> 248) & 0x0f; // get ins nibble
+    a = (a >> 248) & 0x0f; // get outs-length nibble
     Output[] memory outs = new Output[](a);
     if (txType == TxType.Consolidate && ins.length <= outs.length) {
       revert("invalide consolidate");
     }
     for (i = 0; i < outs.length; i++) {
-        offset = parseOutput(txType, _txData, i, offset, outs);
+      offset = parseOutput(txType, _txData, i, offset, outs);
     }
     txn = Tx(txType, ins, outs);
   }
