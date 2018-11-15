79a0626de95ab9b18fa2a5d4641d17abf5d5cb94
Jan <jan@parseclabs.org>
2018-11-15 20:34:52 +0100

    Submit period

---
+++ contracts/Bridge.sol
@@ -0,0 +1,110 @@
+
+/**
+ * Copyright (c) 2018-present, Leap DAO (leapdao.org)
+ *
+ * This source code is licensed under the Mozilla Public License, version 2,
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+pragma solidity 0.4.24;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+import "./MintableToken.sol";
+import "./PriorityQueue.sol";
+
+contract Bridge is Ownable {
+
+  using SafeMath for uint256;
+
+  modifier onlyOperator() {
+    require(msg.sender == operator, "Tried to call a only-operator function from non-operator");
+    _;
+  }
+
+  struct Period {
+    bytes32 parent; // the id of the parent node
+    uint32 height;  // the height of last block in period
+    uint32 parentIndex; //  the position of this node in the Parent's children list
+    uint32 timestamp;
+    bytes32[] children; // unordered list of children below this node
+  }
+
+  bytes32 public constant GENESIS = 0x4920616d207665727920616e6772792c20627574206974207761732066756e21;
+
+  bytes32 public tipHash; // hash of first period that has extended chain to some height
+  uint256 public parentBlockInterval; // how often epochs can be submitted max
+  uint64 public lastParentBlock; // last ethereum block when epoch was submitted
+  address public operator; // the operator of the plasma chain (can be a contract)
+  uint256 public maxReward; // max reward per period
+
+  MintableToken public nativeToken; // plasma native token
+
+  mapping(bytes32 => Period) public periods;
+
+  constructor(
+    uint256 _parentBlockInterval,
+    uint256 _maxReward,
+    MintableToken _nativeToken
+  ) public {
+    // init genesis preiod
+    Period memory genesisPeriod = Period({
+      parent: GENESIS,
+      height: 1,
+      timestamp: uint32(block.timestamp),
+      parentIndex: 0,
+      children: new bytes32[](0)
+    });
+    tipHash = GENESIS;
+    periods[tipHash] = genesisPeriod;
+    parentBlockInterval = _parentBlockInterval;
+    lastParentBlock = uint64(block.number);
+    maxReward = _maxReward;
+    nativeToken = _nativeToken;
+    nativeToken.init(address(this));
+  }
+
+  function setOperator(address _operator) public onlyOwner {
+    operator = _operator;
+  }
+
+  function submitPeriod(bytes32 _prevHash, bytes32 _root) public onlyOperator {
+
+    require(periods[_prevHash].parent > 0, "Parent node should exist");
+    require(periods[_root].height == 0, "Given root shouldn't be submitted yet");
+
+    // calculate height
+    uint256 newHeight = periods[_prevHash].height + 1;
+    // do some magic if chain extended
+    if (newHeight > periods[tipHash].height) {
+      // new periods can only be submitted every x Ethereum blocks
+      require(block.number >= lastParentBlock + parentBlockInterval, 
+        "Tried to submit new period too soon");
+      tipHash = _root;
+      lastParentBlock = uint64(block.number);
+    }
+    // store the period
+    Period memory newPeriod = Period({
+      parent: _prevHash,
+      height: uint32(newHeight),
+      timestamp: uint32(block.timestamp),
+      parentIndex: uint32(periods[_prevHash].children.push(_root) - 1),
+      children: new bytes32[](0)
+    });
+    periods[_root] = newPeriod;
+
+    // distribute rewards
+    uint256 totalSupply = nativeToken.totalSupply();
+    uint256 stakedSupply = nativeToken.balanceOf(operator);
+    uint256 reward = maxReward;
+    if (stakedSupply >= totalSupply.div(2)) {
+      // 4 x br x as x (ts - as)
+      // -----------------------
+      //        ts x ts
+      reward = totalSupply.sub(stakedSupply).mul(stakedSupply).mul(maxReward).mul(4).div(totalSupply.mul(totalSupply));
+    }
+    nativeToken.mint(operator, reward);
+  }
+  
+}
---
+++ test/bridge.js
@@ -0,0 +1,84 @@
+
+/**
+ * Copyright (c) 2018-present, Leap DAO (leapdao.org)
+ *
+ * This source code is licensed under the Mozilla Public License, version 2,
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+import EVMRevert from './helpers/EVMRevert';
+import { Period, Block, Tx, Input, Output, Outpoint } from 'leap-core';
+import chai from 'chai';
+import chaiBigNumber from 'chai-bignumber';
+import chaiAsPromised from 'chai-as-promised';
+
+const Bridge = artifacts.require('Bridge');
+const MintableToken = artifacts.require('MockMintableToken');
+
+const should = chai
+  .use(chaiAsPromised)
+  .use(chaiBigNumber(web3.BigNumber))
+  .should();
+
+contract('Bridge', (accounts) => {
+  const alice = accounts[0];
+  const bob = accounts[1];
+  const charlie = accounts[2];
+
+  describe('Test', function() {
+    let bridge;
+    let nativeToken;
+    const maxReward = 50;
+    const parentBlockInterval = 0;
+
+    beforeEach(async () => {
+      nativeToken = await MintableToken.new();
+      bridge = await Bridge.new(parentBlockInterval, maxReward, nativeToken.address);
+      await bridge.setOperator(bob);
+      // At this point alice is the owner of bridge and has 10000 tokens
+      // Bob is the bridge operator
+      // Note: all txs in these tests originate from alice unless otherwise specified
+    });
+
+    describe('Submit Period', async () => {
+      it('Operator can submit period', async() => {
+        const prevPeriodHash = await bridge.tipHash();
+        const newPeriodHash = '0x0100000000000000000000000000000000000000000000000000000000000000';
+
+        const bobBalanceBefore = await nativeToken.balanceOf(bob);
+        // this is to assure correct reward calculation
+        bobBalanceBefore.should.be.bignumber.equal(0);
+
+        await bridge.submitPeriod(prevPeriodHash, newPeriodHash, {from: bob}).should.be.fulfilled;
+
+        const newTip = await bridge.tipHash();
+        newTip.should.be.equal(newPeriodHash);
+        const bobBalanceAfter = await nativeToken.balanceOf(bob);
+        const bobBalanceDiff = bobBalanceAfter.minus(bobBalanceBefore);
+        // At this point the total stake is 0 (bob's balance) so bob should receive maxReward
+        bobBalanceDiff.should.be.bignumber.equal(maxReward);
+      });
+
+      it('Non-operator cannot submit period', async () => {
+
+      });
+
+      it('Reverts when submitting exsisting period', async () => {
+
+      });
+
+      it('Reverts when parent does not exsist', async () => {
+
+      });
+
+      it('Reverts when trying to submit before parent block interval passed', async () => {
+
+      });
+
+      it('Period reward is computed correctly', async () => {
+
+      });
+    })
+  });
+
+});
