380143f19c8f469b9b04abc47615ae199a8744c5
Kosta Korenkov <kosta@whoot.me>
2018-12-04 16:35:28 +0100

    tests: add should assertions back

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -2,4 +2,4 @@ import chai from 'chai';
 import chaiBigNumber from 'chai-bignumber';
 import chaiAsPromised from 'chai-as-promised';
 
-chai.use(chaiAsPromised).use(chaiBigNumber(web3.BigNumber));
+chai.use(chaiAsPromised).use(chaiBigNumber(web3.BigNumber)).should();
