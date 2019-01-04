a112bfdcdda448a7d2f33986af10dff7fa2189ff
Johann Barbie <johannbarbie@me.com>
2019-01-04 16:20:45 +0100

    added reward and tax calculation

--- contracts/SwapRegistry.sol
+++ contracts/SwapRegistry.sol
@@ -11,8 +11,11 @@ pragma solidity 0.4.24;
 import "./Bridge.sol";
 import "./Adminable.sol";
 import "./SwapExchange.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract SwapRegistry is Adminable {
+  using SafeMath for uint256;
 
   // Swap related
   event NewExchange(address indexed token, address indexed exchange);
@@ -23,24 +26,29 @@ contract SwapRegistry is Adminable {
 
   // Claim Related
   Bridge bridge;
-  uint256 public taxRate; // as permill (1000 == 100%, 1 == 0.1%)
-  uint256 public inflationCap;
+  uint256 taxRate; // as perMil (1000 == 100%, 1 == 0.1%)
+  uint256 claimsPerYear;
+  uint256 inflationCap; // in perMil (500 = 50% inflation per year)
   mapping(uint256 => uint256) slotToHeight;
 
-  function initialize(address _bridge, address _nativeToken) public initializer {
+  function initialize(address _bridge, address _nativeToken, uint256 _taxRate, uint256 _claimsPerYear) public initializer {
     require(_bridge != 0, "invalid bridge address");
     bridge = Bridge(_bridge);
     require(_nativeToken != 0, "invalid token address");
     // todo: check that this contract is admin of token;
-    idToToken[1] = _nativeToken;
-    tokenCount = 1;
+    idToToken[0] = _nativeToken;
+    tokenCount = 0;
+    taxRate = _taxRate;
+    claimsPerYear = _claimsPerYear;
+    inflationCap = 500;
   }
 
   function claim(uint256 _slotId, bytes32[] memory _roots) public {
     uint256 maxHeight = slotToHeight[_slotId];
     uint256 claimCount = 0;
     for (uint256 i = 0; i < _roots.length; i+= 2) {
-      require(_slotId == uint256(_roots[i+1]), "unexpected slotId");
+      require(_slotId == uint256(_roots[i+1] >> 160), "unexpected slotId");
+      require(msg.sender == address(_roots[i+1]), "unexpected claimant");
       uint256 height;
       (,height ,,) = bridge.periods(keccak256(_roots[i], _roots[i + 1]));
       require(height > maxHeight, "unorderly claim");
@@ -49,8 +57,22 @@ contract SwapRegistry is Adminable {
     }
     slotToHeight[_slotId] = maxHeight;
     // calculate token amount
+    // according to https://ethresear.ch/t/riss-reflexive-inflation-through-staked-supply/3633
+    ERC20Mintable token = ERC20Mintable(idToToken[0]);
+    uint256 staked = token.balanceOf(bridge.operator());
+    uint256 total = token.totalSupply();
+    uint256 reward;
+    if (staked < total.div(2)) {
+      reward = total.mul(inflationCap).div(1000).div(claimsPerYear);
+    } else {
+      reward = staked.mul(total).mul(2).sub(staked.mul(staked).mul(2)).div(total).div(claimsPerYear);
+    }
+    reward = reward.mul(claimCount);
+    total = reward;
+    reward = reward.mul(taxRate).div(1000);
     // mint tokens
-    // transfer tokens
+    token.mint(msg.sender, reward);
+    token.mint(bridge.admin(), total.sub(reward));
   }
 
   function createExchange(address _token) public returns (address) {
@@ -77,6 +99,14 @@ contract SwapRegistry is Adminable {
 
   function getTokenWithId(uint256 _tokenId) public view returns(address) {
     return idToToken[_tokenId];
+  }
+
+  function getClaimsPerYear() public view returns(uint256) {
+    return claimsPerYear;
+  }
+
+  function getTaxRate() public view returns(uint256) {
+    return taxRate;
   }
 
 }
--- test/swapRegistry.js
+++ test/swapRegistry.js
@@ -8,14 +8,18 @@
 
 import ethUtil from 'ethereumjs-util';
 import EVMRevert from './helpers/EVMRevert';
+require('./helpers/setup');
 
 const AdminableProxy = artifacts.require('AdminableProxy');
 const Bridge = artifacts.require('Bridge');
 const SwapRegistry = artifacts.require('SwapRegistry');
-const SimpleToken = artifacts.require('SimpleToken');
+const MintableToken = artifacts.require('MintableToken');
 
 contract('SwapRegistry', (accounts) => {
   const bob = accounts[1];
+  const taxRate = 0.5;
+  const claimPerYear = 100000;
+  const inflationCap = 0.5;
 
   describe('Test', () => {
     let bridge;
@@ -25,7 +29,7 @@ contract('SwapRegistry', (accounts) => {
     const parentBlockInterval = 0;
 
     beforeEach(async () => {
-      nativeToken = await SimpleToken.new();
+      nativeToken = await MintableToken.new();
 
       const bridgeCont = await Bridge.new();
       let data = await bridgeCont.contract.initialize.getData(parentBlockInterval);
@@ -36,27 +40,94 @@ contract('SwapRegistry', (accounts) => {
       await proxy.applyProposal(data, {from: accounts[2]}).should.be.fulfilled;
 
       swapRegistry = await SwapRegistry.new();
-      data = await swapRegistry.contract.initialize.getData(bridge.address, nativeToken.address);
+      data = await swapRegistry.contract.initialize.getData(bridge.address, nativeToken.address, taxRate * 1000, claimPerYear);
       proxy = await AdminableProxy.new(swapRegistry.address, data,  {from: accounts[2]});
       swapRegistry = SwapRegistry.at(proxy.address);
+
+      await nativeToken.addMinter(swapRegistry.address);
 
     });
 
     describe('Period claim', async () => {
 
-      it('period submission can be claimed', async () => {
+      it('should receive inflation cap if less than 50% staked', async () => {
         const prevPeriodHash = await bridge.tipHash();
 
         const txRoot = '0x0101010101010101010101010101010101010101010101010101010101010101';
-        const slotId = '0x0000000000000000000000000000000000000000000000000000000000000003';
+        const oracleRoot = `0x000000000000000000000003${bob.replace('0x', '')}`;
         const buffer = Buffer.alloc(64, 0);
         buffer.write(txRoot.replace('0x', ''), 'hex');
-        buffer.write(slotId.replace('0x', ''), 32, 'hex');
+        buffer.write(oracleRoot.replace('0x', ''), 32, 'hex');
         const newPeriodHash = `0x${ethUtil.keccak256(buffer).toString('hex')}`;
 
         await bridge.submitPeriod(prevPeriodHash, newPeriodHash, {from: bob}).should.be.fulfilled;
 
-        await swapRegistry.claim(3, [txRoot, slotId]).should.be.fulfilled;
+        const bobBalBefore = await nativeToken.balanceOf(bob);
+        const taxBalBefore = await nativeToken.balanceOf(accounts[2]);
+        const total = (await nativeToken.totalSupply()).toNumber();
+
+        await swapRegistry.claim(3, [txRoot, oracleRoot], {from: bob}).should.be.fulfilled;
+
+        const bobBalAfter = await nativeToken.balanceOf(bob);
+        const taxBalAfter = await nativeToken.balanceOf(accounts[2]);
+        const reward = (total * inflationCap) / claimPerYear;
+        assert.equal(taxBalBefore.add(reward * taxRate).toNumber(), taxBalAfter.toNumber());
+        assert.equal(bobBalBefore.add(reward - (reward * taxRate)).toNumber(), bobBalAfter.toNumber());
+
+        await swapRegistry.claim(3, [txRoot, oracleRoot], {from: bob}).should.be.rejectedWith(EVMRevert);
+
+        assert.equal(reward * claimPerYear, total * inflationCap);
+      });
+
+      it('should receive no reward if all staked', async () => {
+        await nativeToken.transfer(bob, 1000000000000);
+        const prevPeriodHash = await bridge.tipHash();
+        const txRoot = '0x0202020202020202020202020202020202020202020202020202020202020202';
+        const oracleRoot = `0x000000000000000000000003${bob.replace('0x', '')}`;
+        const buffer = Buffer.alloc(64, 0);
+        buffer.write(txRoot.replace('0x', ''), 'hex');
+        buffer.write(oracleRoot.replace('0x', ''), 32, 'hex');
+        const newPeriodHash = `0x${ethUtil.keccak256(buffer).toString('hex')}`;
+
+        await bridge.submitPeriod(prevPeriodHash, newPeriodHash, {from: bob}).should.be.fulfilled;
+
+        const bobBalBefore = await nativeToken.balanceOf(bob);
+        const taxBalBefore = await nativeToken.balanceOf(accounts[2]);
+
+        await swapRegistry.claim(3, [txRoot, oracleRoot], {from: bob}).should.be.fulfilled;
+
+        const bobBalAfter = await nativeToken.balanceOf(bob);
+        const taxBalAfter = await nativeToken.balanceOf(accounts[2]);
+
+        assert.equal(taxBalBefore.toNumber(), taxBalAfter.toNumber());
+        assert.equal(bobBalBefore.toNumber(), bobBalAfter.toNumber());
+      });
+
+      it('should receive less than inflation cap if more than 50% staked', async () => {
+        await nativeToken.transfer(bob, 750000000000);
+        const prevPeriodHash = await bridge.tipHash();
+        const txRoot = '0x0202020202020202020202020202020202020202020202020202020202020202';
+        const oracleRoot = `0x000000000000000000000003${bob.replace('0x', '')}`;
+        const buffer = Buffer.alloc(64, 0);
+        buffer.write(txRoot.replace('0x', ''), 'hex');
+        buffer.write(oracleRoot.replace('0x', ''), 32, 'hex');
+        const newPeriodHash = `0x${ethUtil.keccak256(buffer).toString('hex')}`;
+
+        await bridge.submitPeriod(prevPeriodHash, newPeriodHash, {from: bob}).should.be.fulfilled;
+
+        const bobBalBefore = await nativeToken.balanceOf(bob);
+        const taxBalBefore = await nativeToken.balanceOf(accounts[2]);
+        const total = (await nativeToken.totalSupply()).toNumber();
+        const staked = bobBalBefore.toNumber();
+
+        await swapRegistry.claim(3, [txRoot, oracleRoot], {from: bob}).should.be.fulfilled;
+
+        const bobBalAfter = await nativeToken.balanceOf(bob);
+        const taxBalAfter = await nativeToken.balanceOf(accounts[2]);
+        const reward = ((2 * staked) / claimPerYear) - ((2 * staked * staked) / (total * claimPerYear));
+
+        assert.equal(taxBalBefore.add(reward * taxRate).toNumber(), taxBalAfter.toNumber());
+        assert.equal(bobBalBefore.add(reward - (reward * taxRate)).toNumber(), bobBalAfter.toNumber());
       });
     });
 
