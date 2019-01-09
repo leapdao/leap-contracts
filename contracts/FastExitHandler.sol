23191f3d4336ca9d0583c5cd8c3d2967cfa4b2bc
Jan <jan@parseclabs.org>
2019-01-09 11:25:36 +0100

    Remove check for youngestinput

--- contracts/FastExitHandler.sol
+++ contracts/FastExitHandler.sol
@@ -29,13 +29,11 @@ contract FastExitHandler is ExitHandler {
     require(msg.value >= exitStake, "Not enough ether sent to pay for exit stake");
     Data memory data;
 
-    (data.parent,,, data.timestamp) = bridge.periods(_proof[0]);
+    (data.parent,,,) = bridge.periods(_proof[0]);
     require(data.parent > 0, "The referenced period was not submitted to bridge");
 
-    if (_youngestInputProof.length > 0) {
-      (data.parent,,, data.timestamp) = bridge.periods(_youngestInputProof[0]);
-      require(data.parent > 0, "The referenced period was not submitted to bridge");
-    }
+    (data.parent,,, data.timestamp) = bridge.periods(_youngestInputProof[0]);
+    require(data.parent > 0, "The referenced period was not submitted to bridge");
 
     // check exiting tx inclusion in the root chain block
     bytes memory txData;
