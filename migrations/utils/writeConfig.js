67b13b5bf2d554487f0832c0f83d3de64653c0db
Kosta Korenkov <kosta@whoot.me>
2019-03-26 14:19:14 +0300

    expose networkdId (#160)

--- migrations/utils/writeConfig.js
+++ migrations/utils/writeConfig.js
@@ -22,6 +22,7 @@ function writeConfig({ bridgeProxy, operatorProxy, exitHandlerProxy }, network) 
     "operatorAddr": operatorProxy.address,
     "exitHandlerAddr": exitHandlerProxy.address,
     "rootNetwork": rootNetwork,
+    "rootNetworkId": 5777,
     "network": network,
     "networkId": networkId,
     "peers": []
