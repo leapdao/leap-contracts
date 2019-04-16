02e94aeac8de974e5d7696f891221d478d13275f
Johann Barbie <johannbarbie@users.noreply.github.com>
2019-04-16 11:57:56 +0200

    Erc1537 (#171)
    * git rid of unused contract
    
    * add ERC1537
    
    * separate interface

---
+++ contracts/IERC1537.sol
@@ -0,0 +1,19 @@
+
+/**
+ * Copyright (c) 2018-present, Leap DAO (leapdao.org)
+ *
+ * This source code is licensed under the Mozilla Public License, version 2,
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+ 
+pragma solidity ^0.5.2;
+
+contract IERC1537 {
+  
+  event DataUpdated(uint256 indexed tokenId, bytes32 oldData, bytes32 newData);
+
+  function readData(uint256 _tokenId) public view returns (bytes32);
+
+  function writeData(uint256 _tokenId, bytes32 _newData) public;
+
+}
