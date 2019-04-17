56d68d5dbeed44618d392f2245ec11d9dd77d8ea
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2019-04-17 14:27:06 +0200

    ERC721Mint

---
+++ contracts/ERC721Mint.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.5.2;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+
+contract ERC721Mint is ERC721 {
+  modifier onlyMinter() {
+    require(msg.sender == 0x0000000000000000000000000000000000000001);
+    _;
+  }
+
+  function mint(address _to, uint256 _tokenId) public onlyMinter {
+    super._mint(_to, _tokenId);
+  }
+}
