228c6a1d86a71fc9ff64559fb834414055cec820
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2019-04-17 14:22:48 +0200

    ERC1537Mint

---
+++ contracts/ERC1537Mint.sol
@@ -0,0 +1,32 @@
+pragma solidity 0.5.2;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import './IERC1537.sol';
+
+
+contract ERC1537Min is IERC1537, ERC721 {
+  mapping(uint256 => bytes32) data;
+
+  modifier onlyMinter() {
+    require(msg.sender == 0x0000000000000000000000000000000000000001);
+    _;
+  }
+
+  function mint(address _to, uint256 _tokenId, bytes32 _newData) public onlyMinter {
+    super._mint(_to, _tokenId);
+
+    emit DataUpdated(_tokenId, data[_tokenId], _newData);
+    data[_tokenId] = _newData;
+  }
+
+  function readData(uint256 _tokenId) public view returns (bytes32) {
+    require(_exists(_tokenId));
+    return data[_tokenId];
+  }
+
+  function writeData(uint256 _tokenId, bytes32 _newData) public {
+    require(msg.sender == ownerOf(_tokenId));
+    emit DataUpdated(_tokenId, data[_tokenId], _newData);
+    data[_tokenId] = _newData;
+  }
+}
