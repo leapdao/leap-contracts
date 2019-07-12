42ee6703f70db1d5b157d59b444227b55d6eb6f8
Johann Barbie <johannbarbie@users.noreply.github.com>
2019-07-12 20:12:28 +0200

    add cas bitmap to submitPeriod() (#224)
    * add cas bitmap to submitPeriod()
    
    * update core
    
    * calculate specific amount of sigs needed

--- contracts/PoaOperator.sol
+++ contracts/PoaOperator.sol
@@ -152,12 +152,30 @@ contract PoaOperator is Adminable {
   event Submission(
     bytes32 indexed blocksRoot,
     uint256 indexed slotId,
-    address owner,
+    address indexed owner,
+    bytes32 casRoot,
     bytes32 periodRoot
   );
 
-  function submitPeriod(uint256 _slotId, bytes32 _prevHash, bytes32 _blocksRoot) public {
+  function countSigs(uint256 _sigs, uint256 _epochLength) internal pure returns (uint256 count) {
+    for (uint i = 256; i >= 256 - _epochLength; i--) {
+      count += uint8(_sigs >> i) & 0x01;
+    }
+  }
+
+  // an exact amount of sigs is needed, so that if one is proven to be invalid,
+  // then the amount of signatures drops below the 2/3 quorum => period is deleted
+  function neededSigs(uint256 _epochLength) internal pure returns (uint256 needed) {
+    // if the number of slots has a remainder, add 1
+    //   example: 5, remainder 1, => 3 + 1
+    // if the number of slots has no remainder, use it
+    //   example: 9, remainder 0, => 6 + 0
+    return (_epochLength * 2 / 3) + ((_epochLength * 2 % 3) == 0 ? 0 : 1);
+  }
+
+  function submitPeriod(uint256 _slotId, bytes32 _prevHash, bytes32 _blocksRoot, bytes32 _cas) public {
     require(_slotId < epochLength, "Incorrect slotId");
+    require(countSigs(uint256(_cas), epochLength) == neededSigs(epochLength), "incorrect number of sigs");
     Slot storage slot = slots[_slotId];
     require(slot.signer == msg.sender, "not submitted by signerAddr");
     // This is here so that I can submit in the same epoch I auction/logout but not after
@@ -175,7 +193,7 @@ contract PoaOperator is Adminable {
     }
     // cas root
     assembly {
-      mstore(0, 0x0000000000000000000000000000000000000000)
+      mstore(0, _cas)
       mstore(0x20, hashRoot)
       hashRoot := keccak256(0, 0x40)
     }
@@ -206,6 +224,7 @@ contract PoaOperator is Adminable {
       _blocksRoot,
       _slotId,
       slot.owner,
+      _cas,
       hashRoot
     );
   }
