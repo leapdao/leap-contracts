6f9c6bbbce7a40556f2069ef5c196fed4710a153
Johann Barbie <johannbarbie@me.com>
2019-12-13 23:57:30 +0100

    first vote

--- contracts/TokenGovernance.sol
+++ contracts/TokenGovernance.sol
@@ -9,46 +9,141 @@
 pragma solidity 0.5.2;
 
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "./Bridge.sol";
+import "./Vault.sol";
+import "./TxLib.sol";
 
 contract TokenGovernance {
 
   uint256 constant PROPOSAL_STAKE = 5000000000000000000000;
+  uint32 constant PROPOSAL_TIME = 604800; // 60 * 60 * 24 * 7 = 7 days
+  address public mvgAddress;
+  IERC20 public leapToken;
+  Bridge public bridge;
+  Vault public vault;
 
   struct Proposal {
     address initiator;
-    uint256 openTime;
-    mapping(address => uint256) votes;
+    uint32 openTime;
     bool finalized;
+    uint256 yesVotes;
+    uint256 noVotes;
+    mapping(address => int256) votes;
+    mapping(bytes32 => address) usedTxns;
   }
 
   mapping(bytes32 => Proposal) public proposals;
-  address public mvgAddress;
-  address public leapAddr;
 
-  event ProposalRegistered(bytes32 indexed ipfsHash, address indexed initiator);
-  event VoiceCasted(uint256 indexed ipfsHash, address indexed subject, uint256 weight);
-  event VoiceChallenged(uint256 indexed ipfsHash, address indexed subject, address challenger);
-  event ProposalFinalized(bytes32 indexed ipfsHash, bool isApproved);
+  event ProposalRegistered(bytes32 indexed proposalHash, address indexed initiator);
+  event VoiceCasted(bytes32 indexed proposalHash, address indexed subject, uint256 weight);
+  event VoiceChallenged(bytes32 indexed proposalHash, address indexed subject, address challenger);
+  event ProposalFinalized(bytes32 indexed proposalHash, bool isApproved);
 
-
-  constructor(address _mvgAddress, address _leapAddr) public {
+  constructor(address _mvgAddress, IERC20 _leapToken, Vault _vault) public {
     mvgAddress = _mvgAddress;
-    leapAddr = _leapAddr;
+    leapToken = _leapToken;
+    vault = _vault;
+    bridge = _vault.bridge();
+  }
+
+  function _revertVote(Proposal storage proposal, bytes32 txHash) internal {
+    address signer = proposal.usedTxns[txHash];
+    delete proposal.usedTxns[txHash];
+    int256 votes = proposal.votes[signer];
+    delete proposal.votes[signer];
+    if (votes > 0) {
+      proposal.yesVotes -= uint256(votes);
+    } else {
+      proposal.noVotes -= uint256(votes * -1);
+    }
   }
 
   function registerProposal(bytes32 _proposalHash) public {
     // make sure same proposals hasn't been opened before
     require(proposals[_proposalHash].openTime == 0, "proposal already exists");
-
     // get instance of token contract and pull proposal stake
-    IERC20 leapToken = IERC20(leapAddr);
     leapToken.transferFrom(msg.sender, address(this), PROPOSAL_STAKE);
-
     // create a new proposal in storage
-    proposals[_proposalHash] = Proposal(msg.sender, now, false);    
-
+    proposals[_proposalHash] = Proposal(msg.sender, uint32(now), false, 0, 0);
     // emit event for frontend
     emit ProposalRegistered(_proposalHash, msg.sender);
+  }
+
+  function castVote(bytes32 _proposalHash, bytes32[] memory _proof, uint8 _outputIndex, bool isYes) public {
+    Proposal memory proposal = proposals[_proposalHash];
+    require(proposal.openTime > 0, "proposal does not exist");
+    require(proposal.finalized == false, "proposal already finalized");
+    uint32 timestamp;
+    (, timestamp,,) = bridge.periods(_proof[0]);
+    require(timestamp > 0, "The referenced period was not submitted to bridge");
+    require(timestamp <= proposal.openTime, "The transaction was submitted after the vote open time");
+    
+    bytes memory txData;
+    bytes32 txHash;
+    (, txHash, txData) = TxLib.validateProof(96, _proof);
+
+    // parse tx and check if it is usable for voting
+    TxLib.Tx memory tx = TxLib.parseTx(txData);
+    TxLib.Output memory out = tx.outs[_outputIndex];
+    require(out.value > 0, "UTXO has no value");
+    require(out.owner == msg.sender, "msg.sender not owner of utxo");
+    // TODO: fix
+    require(address(leapToken) == vault.getTokenAddr(out.color), "not Leap UTXO");
+
+    if (proposals[_proposalHash].votes[msg.sender] != 0) {
+      // TODO: clean up previous vote
+      _revertVote(proposals[_proposalHash], txHash);
+    }
+    if (isYes) {
+      proposals[_proposalHash].yesVotes += out.value;
+      proposals[_proposalHash].votes[msg.sender] = int256(out.value);
+    } else {
+      proposals[_proposalHash].noVotes += out.value;
+      proposals[_proposalHash].votes[msg.sender] = int256(out.value) * -1;
+    }
+    proposals[_proposalHash].usedTxns[txHash] = msg.sender;
+    emit VoiceCasted(_proposalHash, msg.sender, out.value);
+  }
+
+  function challengeUTXO(bytes32 _proposalHash, bytes32[] memory _proof, uint8 _inputIndex, address _signer) public {
+    uint32 timestamp;
+    (, timestamp,,) = bridge.periods(_proof[0]);
+    require(timestamp > 0, "The referenced period was not submitted to bridge");
+    require(timestamp <= proposals[_proposalHash].openTime, "The transaction was submitted after the vote open time");
+    
+    bytes memory txData;
+    bytes32 txHash;
+    (, txHash, txData) = TxLib.validateProof(96, _proof);
+
+    // parse tx
+    TxLib.Tx memory txn = TxLib.parseTx(txData);
+    // checking that the transactions has been used in a vote
+    address signer = proposals[_proposalHash].usedTxns[txn.ins[_inputIndex].outpoint.hash];
+    // note: we don't check the output index here, assume that each transactions is only used for 1 vote
+    require(signer != address(0), "prevout check failed on hash");
+
+    // substract vote
+    _revertVote(proposals[_proposalHash], txHash);
+    emit VoiceChallenged(_proposalHash, signer, msg.sender);
+  }
+
+  function finalizeProposal(bytes32 _proposalHash) public {
+    Proposal memory proposal = proposals[_proposalHash];
+    require(proposal.finalized == false, "already finalized");
+    require(proposal.openTime > 0, "proposal does not exist");
+    require(proposal.openTime + PROPOSAL_TIME < uint32(now), "proposal time not exceeded");
+    // can't delete full mappings
+    // delete proposals[_proposalHash].votes;
+    // delete proposals[_proposalHash].usedTxns;
+    proposals[_proposalHash].finalized = true;
+    // return stake
+    if (proposal.yesVotes > proposal.noVotes) {
+      leapToken.transfer(proposal.initiator, PROPOSAL_STAKE);
+      emit ProposalFinalized(_proposalHash, true);
+    } else {
+      leapToken.transfer(mvgAddress, PROPOSAL_STAKE);
+      emit ProposalFinalized(_proposalHash, false);
+    }
   }
 
 }
--- test/tokenGovernance.js
+++ test/tokenGovernance.js
@@ -1,22 +1,51 @@
 import chai from 'chai';
-import EVMRevert from './helpers/EVMRevert';
+import { Tx } from 'leap-core';
+import { EVMRevert, submitNewPeriodWithTx } from './helpers';
 
+const AdminableProxy = artifacts.require('AdminableProxy');
 const NativeToken = artifacts.require('NativeToken');
+const Bridge = artifacts.require('Bridge');
+const Vault = artifacts.require('Vault');
 const TokenGovernance = artifacts.require('TokenGovernance');
 
 chai.use(require('chai-as-promised')).should();
 
 contract('TokenGovernance', (accounts) => {
-
+  let bridge;
+  let vault;
   let gov;
   let leapToken;
+  let proxy;
   const proposalHash = '0x1122334411223344112233441122334411223344112233441122334411223344';
   const proposalStake = '5000000000000000000000';
+  const parentBlockInterval = 0;
+  const alice = accounts[0];
+  const bob = accounts[1];
 
-  beforeEach(async () => {
+  const submitNewPeriod = txs => submitNewPeriodWithTx(txs, bridge, { from: bob });
+
+  before(async () => {
     leapToken = await NativeToken.new("Leap Token", "Leap", 18);
     leapToken.mint(accounts[0], proposalStake);
-    gov = await TokenGovernance.new(accounts[0], leapToken.address);
+
+    const bridgeCont = await Bridge.new();
+    let data = await bridgeCont.contract.methods.initialize(parentBlockInterval).encodeABI();
+    proxy = await AdminableProxy.new(bridgeCont.address, data, {from: accounts[2]});
+    bridge = await Bridge.at(proxy.address);
+
+    data = await bridge.contract.methods.setOperator(bob).encodeABI();
+    await proxy.applyProposal(data, {from: accounts[2]}).should.be.fulfilled;
+
+    const vaultCont = await Vault.new();
+    data = await vaultCont.contract.methods.initialize(bridge.address).encodeABI();
+    proxy = await AdminableProxy.new(vaultCont.address, data,  {from: accounts[2]});
+    vault = await Vault.at(proxy.address);
+
+    // register first token
+    data = await vault.contract.methods.registerToken(leapToken.address, 0).encodeABI();
+    await proxy.applyProposal(data, {from: accounts[2]}).should.be.fulfilled;
+
+    gov = await TokenGovernance.new(accounts[0], leapToken.address, vault.address);
   });
 
 
@@ -26,18 +55,28 @@ contract('TokenGovernance', (accounts) => {
   });
 
   it('should allow to create proposal', async () => {
+    const amount = 34567000000;
+    const depositTx = Tx.deposit(123, amount, alice);
+
+    const period1 = await submitNewPeriod([depositTx]);
+    const inputProof = period1.proof(depositTx);
+
     // allow gov contract to pull funds
     leapToken.approve(gov.address, proposalStake);
     // register proposal
     await gov.registerProposal(proposalHash);
     // read proposal
-    const rsp = await gov.proposals(proposalHash);
+    let rsp = await gov.proposals(proposalHash);
     assert(rsp.openTime > 0);
 
     // check that same proposal can not be rigestered twice
     leapToken.mint(accounts[0], proposalStake);
     leapToken.approve(gov.address, proposalStake);
     await gov.registerProposal(proposalHash).should.be.rejectedWith(EVMRevert);
+
+    await gov.castVote(proposalHash, inputProof, 0, true);
+    rsp = await gov.proposals(proposalHash);
+    assert.equal(rsp.yesVotes, amount);
   });
 
 });
