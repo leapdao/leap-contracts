4e9899df0189fefe8492bf06d811457d41f7064e
Johann Barbie <johannbarbie@me.com>
2020-04-16 20:06:21 +0200

    added safemath

--- contracts/misc/PaymentSplitter.sol
+++ contracts/misc/PaymentSplitter.sol
@@ -6,7 +6,10 @@
  */
 pragma solidity 0.5.2;
 
+import "../../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 contract PaymentSplitter {
+  using SafeMath for uint256;
 
  /**
   * Transfers ether to multiple recipients as specified by _recepients and _splits arrays
@@ -18,16 +21,16 @@ contract PaymentSplitter {
     address payable[] memory _recipients,
     uint256[] memory _splits
   ) public payable {
-    uint256 amount = msg.value;
+    require(_recipients.length > 0, "no data for split");
     require(_recipients.length == _splits.length, "splits and recipients should be of the same length");
 
     uint256 sumShares = 0;
     for (uint i = 0; i < _recipients.length; i++) {
-      sumShares += _splits[i];
+      sumShares = sumShares.add(_splits[i]);
     }
 
     for (uint i = 0; i < _recipients.length - 1; i++) {
-      _recipients[i].transfer(amount * _splits[i] / sumShares);
+      _recipients[i].transfer(msg.value.mul(_splits[i]).div(sumShares));
     }
     // flush the rest, so that we don't have rounding errors or stuck funds
     _recipients[_recipients.length - 1].transfer(address(this).balance);
